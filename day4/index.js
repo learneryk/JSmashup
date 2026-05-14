06ffc6f984faea891316322f1b280aca3dfd4d05
Mohamed Afsal <mohddafsal@gmail.com>
2026-05-14 15:30:10 +0530

    day 4

---
+++ day4/index.js
@@ -0,0 +1,14 @@
+const randomValue = Math.floor((Math.random() * (100 - 1) + 1));
+console.log(randomValue)
+
+let userScore = "75"
+//const stringValue = String(randomValue);
+userScore = parseInt(userScore);
+
+console.log(randomValue - userScore)
+console.log(Math.round(Math.sqrt(userScore)))
+console.log(Math.PI)
+
+
+let string = "50";
+console.log(string * 2)
